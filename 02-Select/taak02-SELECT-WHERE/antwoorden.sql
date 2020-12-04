a9ce7b39d6314dd0bf6d74f1b4b252f1665b7172
Dain Vugs <dain.vugs@rocva.nl>
2020-12-04 12:46:35 +0100

    done

--- 02-Select/taak02-SELECT-WHERE/antwoorden.sql
+++ 02-Select/taak02-SELECT-WHERE/antwoorden.sql
@@ -7,13 +7,14 @@ SELECT * FROM `jaar2015` WHERE country='Netherlands'
 -- Opdracht 4 zelfde antwoord als 2, klopt dit
 SELECT * FROM `jaar2016` WHERE country='Netherlands'
 -- Opdracht 5
-
+SELECT * FROM `jaar2015` WHERE region='North America'
 -- Opdracht 6 
-
+SELECT country FROM `jaar2016` WHERE rank=25
 -- Opdracht 7 
-
+SELECT country FROM `jaar2016` WHERE score>7078
 -- Opdracht 8
-
+SELECT country, region FROM `jaar2016`  WHERE rank<=5
 -- Opdracht 9
-
+SELECT score FROM `jaar2015`  WHERE country='Ireland' 
 -- Opdracht 10
+SELECT country FROM `jaar2016`  WHERE region='Latin America and Caribbean' 
